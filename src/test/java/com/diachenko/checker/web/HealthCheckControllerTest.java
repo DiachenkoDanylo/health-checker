2222abf03fdc50caf2518e167d0604c70b291a96
Diachenko Danylo <moyo6699@gmail.com>
2026-03-08 20:57:29 +0200

    fix: switch to @SpringBootTest for MVC tests instead of @WebMvcTest

--- src/test/java/com/diachenko/checker/web/HealthCheckControllerTest.java
+++ src/test/java/com/diachenko/checker/web/HealthCheckControllerTest.java
@@ -11,9 +11,13 @@ import com.diachenko.checker.controller.HealthCheckController;
 import com.diachenko.checker.model.entity.AppUser;
 import com.diachenko.checker.service.MonitoredUrlService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -22,16 +26,18 @@ import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(HealthCheckController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles({"standalone", "h2"})
 class HealthCheckControllerTest {
+
+    AppUser appUser;
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockitoBean
     private MonitoredUrlService monitoredUrlService;
-
-    AppUser appUser;
 
     @BeforeEach
     void setUp() {
